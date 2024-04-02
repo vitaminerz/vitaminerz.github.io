d3c8c820ba64ab3f915bc8b765b110261000ab2f
vitaminerz <162372576+vitaminerz@users.noreply.github.com>
2024-04-02 01:52:47 -0400

    v1.02

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -19,9 +19,9 @@ const Banner = () => {
         transition={{ duration: 0.5, delay: 0.7 }}
         className="text-4xl lgl:text-6xl font-titleFont font-semibold flex flex-col"
       >
-        Amine Errazi.
+        Amine Errazi
         <span className="text-textDark mt-2 lgl:mt-4">
-          I build things for the web.
+          Product.
         </span>
       </motion.h1>
       <motion.p
@@ -30,27 +30,24 @@ const Banner = () => {
         transition={{ duration: 0.5, delay: 0.8 }}
         className="text-base md:max-w-[650px] text-textDark font-medium"
       >
-        I am a web developer with 4+ years of experience in React. I have a
-        strong foundation in front-end & back-end development and am skilled in
-        creating user-friendly and responsive web applications using React and
-        its ecosystem.{" "}
-        <a href="https://www.noormohammad.live/" target="_blank">
+        I'm a data-driven Product Strategy and Innovation Unicorn specialized in leading, managing, owning and delivering groundbreaking products from inception to market success.{" "}
+        {/* <a href="https://www.noormohammad.live/" target="_blank">
           {" "}
           <span className="text-textGreen inline-flex relative cursor-pointer h-7 overflow-x-hidden group">
             Learn more
             <span className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
           </span>
-        </a>
+        </a> */}
       </motion.p>
       <a href="https://github.com/noorjsdivs" target="_blank">
-        <motion.button
+        {/* <motion.button
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.9 }}
           className="w-52 h-14 text-sm font-titleFont border border-textGreen rounded-md text-textGreen tracking-wide hover:bg-hoverColor duration-300"
         >
           Check out my project!
-        </motion.button>
+        </motion.button> */}
       </a>
     </section>
   );
