28fb9113a96772c29c1e1eae7ea9fd9ecdc161aa
vitaminerz <162372576+vitaminerz@users.noreply.github.com>
2024-04-02 16:51:35 -0400

    v1.55

--- components/ArchiveCard.tsx
+++ components/ArchiveCard.tsx
@@ -4,13 +4,12 @@ import { RxOpenInNewWindow } from "react-icons/rx";
 interface Props {
   title: string;
   des: string;
-  listItem: string[];
-  link: string;
+  listItem: string[]
 }
 
-const ArchiveCard = ({ title, des, listItem, link }: Props) => {
+const ArchiveCard = ({ title, des, listItem }: Props) => {
   return (
-    <a href={link} target="_blank">
+    <a >
       <div className="w-full h-80 rounded-lg bg-[#112240] p-7 flex flex-col justify-center gap-6 hover:-translate-y-2 transition-transform duration-300 group">
         <div className="flex justify-between items-center">
           <FaRegFolder className="text-4xl text-textGreen" />
